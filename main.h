944147dbdbcc37dc0d6df9b25bc94c2ff9ab15f6
Leane <leaneleboulanger@gmail.com>
2024-01-09 13:32:19 +0000

    Generation of the map with the tiles

--- main.h
+++ main.h
@@ -17,7 +17,8 @@ const int WIN_WIDTH = 800; // 25 tuiles de 32 pixels
 const int WIN_HEIGHT = 576; // 18 tuiles de 32 pixels
 const int SPRITE_SIZE = 32; // sprite de 32px
 const int WALK_SPEED = 2;
-
+const int COL_COUNT = 25; // nombre de case en largeur
+const int ROW_COUNT = 18;// nombre de case en ligne
 
 // namespace
 using namespace sf;
--- map.h
+++ map.h
@@ -10,8 +10,7 @@ using namespace std;
 class Map : public Drawable, public Transformable
 {
 public:
-    bool load(const string& tileset, Vector2u tileSize, const int* tiles,
-        unsigned int width, unsigned int height)
+    bool load(const string& tileset, Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
     {
         // on charge la texture du tileset
         if (!m_tileset.loadFromFile(tileset))
@@ -25,23 +24,23 @@ public:
         for (unsigned int i = 0; i < width; ++i)
             for (unsigned int j = 0; j < height; ++j)
             {
-                // on r?cup?re le num?ro de tuile courant
+                // on r cup re le num ro de tuile courant
                 int tileNumber = tiles[i + j * width];
 
-                // on en d?duit sa position dans la texture du tileset
+                // on en d duit sa position dans la texture du tileset
                 int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
                 int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
 
-                // on r?cup?re un pointeur vers le quad ? d?finir dans le tableau de vertex
+                // on r cup re un pointeur vers le quad   d finir dans le tableau de vertex
                 Vertex* quad = &m_vertices[(i + j * width) * 4];
 
-                // on d?finit ses quatre coins
+                // on d finit ses quatre coins
                 quad[0].position = Vector2f(i * tileSize.x, j * tileSize.y);
                 quad[1].position = Vector2f((i + 1) * tileSize.x, j * tileSize.y);
                 quad[2].position = Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
                 quad[3].position = Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
 
-                // on d?finit ses quatre coordonn?es de texture
+                // on d finit ses quatre coordonn es de texture
                 quad[0].texCoords = Vector2f(tu * tileSize.x, tv * tileSize.y);
                 quad[1].texCoords = Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
                 quad[2].texCoords = Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
